2c7550339f9841a2904d3206ed95aae034e0a54d
José Ríos <jlrd@Joses-MacBook-Air.local>
2026-08-12 17:01:59 -0600

    feat: add basic Python programs

--- basics/my_name.py
+++ basics/my_name.py
@@ -3,7 +3,7 @@ last_name = "Ríos"
 age = 51
 occupation = "Software Engineer"
 
-print("\nTell me a little about you")
+print("\nTell me a little bit about yourself")
 print("\nWhat is your name?: ", name, last_name)
 print("How old are you?:", age)
 print("What is your occupation?", occupation)
